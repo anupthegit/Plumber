e634ba093be566eb54994a54114df30776b1967a
Anup Vasudevan <anupv1310@gmail.com>
2018-02-18 16:41:55 +0100

    Added simple file based persistence for pipeline steps

--- src/SerialPipe.py
+++ src/SerialPipe.py
@@ -1,17 +1,37 @@
+import pickle
+import os
+import hashlib
+
+
 class SerialPipe:
 
-    def __init__(self, pipeline):
-        keys = sorted(pipeline.keys(),
-                      key=lambda step_name: pipeline[step_name]['order'] if 'order' in pipeline[step_name]
+    def __init__(self, pipeline_spec):
+        keys = sorted(pipeline_spec.keys(),
+                      key=lambda step_name: pipeline_spec[step_name]['order'] if 'order' in pipeline_spec[step_name]
                       else -1)
-        values = [pipeline[step_name] for step_name in keys]
+        values = [pipeline_spec[step_name] for step_name in keys]
         self.__pipeline_entries = zip(keys, values)
+        self.__step_outputs = {}
+        self.__id = hashlib.md5(str(pipeline_spec).encode('UTF-8')).hexdigest()
 
     def execute(self):
         prev_output = None
         for entry in self.__pipeline_entries:
             prev_output = self.__execute_pipeline_step(entry[1], prev_output)
+            self.__step_outputs[entry[0]] = prev_output
+            with open(self.__id, 'wb') as f:
+                pickle.dump(self.__step_outputs, f)
         return prev_output
+
+    def get_outputs(self):
+        if self.__step_outputs == {}:
+            if os.path.exists(self.__id):
+                with open(self.__id, 'rb') as f:
+                    self.__step_outputs = pickle.load(f)
+        return self.__step_outputs
+
+    def get_id(self):
+        return self.__id
 
     @staticmethod
     def __execute_pipeline_step(pipeline_step, input_data):
--- test/test_serialPipe.py
+++ test/test_serialPipe.py
@@ -8,7 +8,7 @@ import operator
 class TestSerialPipe(TestCase):
 
     def test_execute_should_run_single_step_with_function_and_args(self):
-        simple_pipeline = {
+        simple_pipeline_spec = {
             'single_step': {
                 'function': operator.add,
                 'args': [
@@ -17,12 +17,12 @@ class TestSerialPipe(TestCase):
                 ]
             }
         }
-        serial_pipe = SerialPipe(simple_pipeline)
+        serial_pipe = SerialPipe(simple_pipeline_spec)
         actual = serial_pipe.execute()
         self.assertEqual('Hello World!', actual)
 
     def test_execute_should_run_single_step_with_function_and_args_dict(self):
-        simple_pipeline = {
+        simple_pipeline_spec = {
             'single_step': {
                 'function': lambda first, second: first + second,
                 'args': {
@@ -31,12 +31,12 @@ class TestSerialPipe(TestCase):
                 }
             }
         }
-        serial_pipe = SerialPipe(simple_pipeline)
+        serial_pipe = SerialPipe(simple_pipeline_spec)
         actual = serial_pipe.execute()
         self.assertEqual('Hello World!', actual)
 
     def test_execute_should_run_each_step_in_specified_order_and_pass_output_to_next_step(self):
-        multi_step_pipeline = {
+        multi_step_pipeline_spec = {
             'step1': {
                 'function': operator.add,
                 'args': [
@@ -54,12 +54,12 @@ class TestSerialPipe(TestCase):
                 'order': 1
             }
         }
-        serial_pipe = SerialPipe(multi_step_pipeline)
+        serial_pipe = SerialPipe(multi_step_pipeline_spec)
         actual = serial_pipe.execute()
         self.assertEqual(144, actual)
 
     def test_execute_each_step_should_accept_previous_step_output_as_input_data_along_with_additional_arguments(self):
-        multi_step_pipeline = {
+        multi_step_pipeline_spec = {
             'step1': {
                 'function': lambda first, second: first + second,
                 'args': {
@@ -76,7 +76,7 @@ class TestSerialPipe(TestCase):
                 'order': 1
             },
             'step3': {
-                'function': lambda x: x*2,
+                'function': lambda x: x * 2,
                 'order': 2
             },
             'step4': {
@@ -88,9 +88,75 @@ class TestSerialPipe(TestCase):
                 'order': 3
             },
         }
-        serial_pipe = SerialPipe(multi_step_pipeline)
+        serial_pipe = SerialPipe(multi_step_pipeline_spec)
         actual = serial_pipe.execute()
         self.assertEqual('48x1x2', actual)
+
+    def test_get_id_should_return_unique_non_random_id(self):
+        pipeline_spec1 = {
+            'single_step': {
+                'function': operator.add,
+                'args': [
+                    'Hello ',
+                    'World!'
+                ]
+            }
+        }
+        pipeline_spec2 = {
+            'single_step': {
+                'function': lambda first, second: first + second,
+                'args': {
+                    'first': 'Hello ',
+                    'second': 'World!'
+                }
+            }
+        }
+        pipeline1 = SerialPipe(pipeline_spec1)
+        pipeline11 = SerialPipe(pipeline_spec1)
+        pipeline2 = SerialPipe(pipeline_spec2)
+        self.assertEqual(pipeline1.get_id(), pipeline11.get_id())
+        self.assertNotEqual(pipeline1.get_id(), pipeline2.get_id())
+
+    def test_get_outputs_should_get_saved_step_outputs(self):
+        multi_step_pipeline_spec = {
+            'step1': {
+                'function': operator.add,
+                'args': [
+                    'Hello ',
+                    'World!'
+                ],
+                'order': 0
+            },
+            'step3': {
+                'function': lambda x: x ** 2,
+                'order': 2
+            },
+            'step2': {
+                'function': len,
+                'order': 1
+            }
+        }
+        serial_pipe = SerialPipe(multi_step_pipeline_spec)
+        serial_pipe.execute()
+        step_outputs = serial_pipe.get_outputs()
+        self.assertEqual('Hello World!', step_outputs['step1'])
+        self.assertEqual(12, step_outputs['step2'])
+        self.assertEqual(144, step_outputs['step3'])
+
+    def test_get_outputs_should_get_saved_outputs_from_persistent_storage_based_on_id(self):
+        pipeline_spec = {
+            'single_step': {
+                'function': operator.add,
+                'args': [
+                    'Hello ',
+                    'World!'
+                ]
+            }
+        }
+        pipeline1 = SerialPipe(pipeline_spec)
+        pipeline2 = SerialPipe(pipeline_spec)
+        pipeline1.execute()
+        self.assertEqual('Hello World!', pipeline2.get_outputs()['single_step'])
 
 
 if __name__ == '__main__':
